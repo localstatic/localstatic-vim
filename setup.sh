865a24b89e552b3f6a59ba757c8d1e08382f9008
Morgan Terry <localstatic@localstatic.com>
2015-07-08 23:40:34 -0600

    Set proper working dir for setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+dir=`pwd`
+
 # Set up submodules
 git submodule init
 git submodule update
 
 # Symlink vimrc & vim into your home directory (i.e. ~/.vimrc and ~/.vim)
-dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd
 ln -s $dir/vimrc .vimrc
 ln -s $dir/vim .vim
